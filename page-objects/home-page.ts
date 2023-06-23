2534b3e76f1d941844c746a9df00cefdd67aaa06
ngocdd <ngocdd94@gmail.com>
2023-06-24 00:06:37 +0700

    update locators

--- page-objects/home-page.ts
+++ page-objects/home-page.ts
@@ -4,57 +4,69 @@ import { Page, expect, Locator } from "@playwright/test";
 export default class HomePage{
     // list elements
     readonly page:Page;
-    readonly lmMenu: Locator;
-    readonly lmBook: Locator;
+    readonly mnuLearningMaterial: Locator;
+    readonly mnuBook: Locator;
     readonly btnAddBook: Locator;
     readonly txtChapterName: Locator;
-    readonly FooterDialogConfirm__buttonSave: Locator;
-    readonly dialogSuccess: Locator;
+    readonly btnSave: Locator;
+    readonly dlgSuccessPopup: Locator;
     readonly btnAddChapter: Locator;
     readonly btnChapterSave: Locator;
     readonly btnAddTopic: Locator;
     readonly btnAddLO: Locator;
-    readonly listLO: Locator;
-    readonly LO: Locator;
+    readonly lstLO: Locator;
+    readonly ddlLO: Locator;
     readonly txtTopicName: Locator;
     readonly txtLOName: Locator;
     readonly btnQuestions: Locator;
-    readonly createQuesiton: Locator;s
-
-
+    readonly btnCreateQuesiton: Locator;
+    readonly ddlQuestionType: Locator;
+    readonly txtQuestionDescription: Locator;
+    readonly txtQuestionAnswers: any;
+    readonly txtQuestionExplanation: Locator;
+    readonly btnDeleteQuestion: Locator;
+    readonly rdoCorrectAnswer: any;
+    readonly btnAddAnswer: Locator;
 
     // constructor
     constructor(page:Page){
         this.page = page;
-        this.lmMenu = page.getByTestId('MenuGroup__root').getByText('Learning Material');
-        this.lmBook = page.getByLabel('Book', {exact: true});
+        this.mnuLearningMaterial = page.getByTestId('MenuGroup__root').getByText('Learning Material');
+        this.mnuBook = page.getByLabel('Book', {exact: true});
         this.btnAddBook = page.getByTestId('AddBook__addButton');
         this.txtChapterName = page.getByTestId('ChapterForm__root').getByTestId('TextFieldHF__input');
         this.txtTopicName = page.getByTestId('TopicForm__root').getByTestId('TextFieldHF__input');
-        this.FooterDialogConfirm__buttonSave = page.getByTestId('FooterDialogConfirm__buttonSave');
-        this.dialogSuccess = page.getByTestId('SnackbarBase__content');
+        this.btnSave = page.getByTestId('FooterDialogConfirm__buttonSave');
+        this.dlgSuccessPopup = page.getByTestId('SnackbarBase__content');
         this.btnAddChapter = page.getByTestId('ChapterForm__visibleFormControl');
         this.btnChapterSave = page.getByTestId('ChapterForm__submit');
         this.btnAddTopic = page.getByTestId('TopicList__createTopic');
         this.btnAddLO = page.getByTestId('LOAndAssignment__addLOs');
-        this.listLO = page.getByTestId('SelectHF__select');
-        this.LO = page.getByRole('option').getByText('Learning Objective');
+        this.lstLO = page.getByTestId('SelectHF__select');
+        this.ddlLO = page.getByRole('option').getByText('Learning Objective');
         this.txtLOName = page.getByTestId('TextFieldHF__input');
         this.btnQuestions = page.getByTestId('QuestionListSectionHeader__action').getByTestId('ActionPanel__trigger');
-        this.createQuesiton = page.getByLabel('createQuestion', {exact: true});
-    }
+        this.btnCreateQuesiton = page.getByLabel('createQuestion', {exact: true});
+        this.btnDeleteQuestion = page.getByTestId('QuizAnswer__DeleteButton');
+        this.ddlQuestionType = page.getByTestId('QuizTypeSelect__root');
+        this.txtQuestionDescription = page.getByTestId('Editor__content').filter({hasText: 'Question Description'}).getByTestId('Editor__draftEditor');
+        this.txtQuestionAnswers = (answerNumber = 1)=>{return page.getByTestId('Editor__content').filter({hasText: `Answer ${answerNumber}`}).getByTestId('Editor__draftEditor');}
+        this.txtQuestionExplanation = page.getByTestId('Editor__content').filter({hasText: 'Explanation'}).getByTestId('Editor__draftEditor');
+        this.rdoCorrectAnswer = (answerNumber = 1)=>{return page.getByTestId('QuizMCQAnswerItem__root').filter({hasText: `Answer ${answerNumber}`}).getByTestId('QuizMCQRadioHF__radio')};
+        this.btnAddAnswer = page.getByTestId('QuizAnswerList__btnAddAnswer');
+    }   
 
     // 
     async gotoBookManagement(){
-        await this.lmMenu.click();
-        await this.lmBook.click();
+        await this.mnuLearningMaterial.click();
+        await this.mnuBook.click();
     }
 
     async addNewBook(bookName:string){
         await this.btnAddBook.click();
         await this.txtChapterName.fill(bookName);
-        await this.FooterDialogConfirm__buttonSave.click();
-        console.log(this.dialogSuccess.textContent());
+        await this.btnSave.click();
+        console.log(this.dlgSuccessPopup.textContent());
     }
 
     async gotoBookDetail(bookId?:string){
@@ -62,7 +74,6 @@ export default class HomePage{
             await this.gotoBookManagement();
             await this.page.goto(`${process.env.BASE_URL}syllabus/books/${bookId}/show`);
         }
-
     }
 
     async addNewChapter(){
@@ -74,20 +85,41 @@ export default class HomePage{
     async addNewTopic(){
         await this.btnAddTopic.click();
         await this.txtTopicName.fill('topic Name');
-        await this.FooterDialogConfirm__buttonSave.click();
+        await this.btnSave.click();
     }
 
     async addNewLO(){
         await this.btnAddLO.click();
-        await this.listLO.click();
-        await this.LO.click();
+        await this.lstLO.click();
+        await this.ddlLO.click();
         await this.txtLOName.fill('abc');
-        await this.FooterDialogConfirm__buttonSave.click();
+        await this.btnSave.click();
     }
 
     async addNewQuestion(){
         await this.btnQuestions.click();
-        await this.createQuesiton.click();
+        await this.btnCreateQuesiton.click();
+    }
+
+    async addNewAnswer(){
+        await this.btnAddAnswer.click();
+    }
+
+    async selectCorrectAnswer(answerNumber = 1){
+        await this.rdoCorrectAnswer(answerNumber).click();
+    }
+
+    async inputQuestionInformation(questionDescription: string, QuestionExplanation: string){;
+        await this.txtQuestionDescription.fill(questionDescription);
+        await this.txtQuestionExplanation.fill(QuestionExplanation);
+    }
+
+    async inputAnswers(answerNumber = 1, answerDescription: string){
+        await this.txtQuestionAnswers(answerNumber).fill(answerDescription);
+    }
+
+    async saveAction(){
+        await this.btnAddAnswer.click();
     }
 
 }
--- tests/test-home-page.ts
+++ tests/test-home-page.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import HomePage from '../page-objects/home-page';
 import LoginPage from '../page-objects/login-page';
-
+import * as fs from 'fs-extra';
 // const homePage: HomePage;
 // const loginPage: LoginPage; 
 
@@ -16,4 +16,25 @@ test('test Elements page', async ({ page }) => {
   await homePage.addNewLO();
   await homePage.addNewQuestion();
   await page.pause();
+});
+
+test('test 2',async () => {
+  
+
+  const filePath = './utils/test-data/question.json';
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      console.log('Parsed JSON:', jsonData.question);
+      // Perform fursther operations with the jsonData object
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  });
 });
