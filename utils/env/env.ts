7c5ba676289d0a061ad1c98d0a6595292ad9f81a
ngocdd <ngocdd94@gmail.com>
2023-06-29 10:16:14 +0700

    update git ignore

--- utils/env/env.ts
+++ utils/env/env.ts
@@ -1,17 +1,21 @@
 export function setEnv (){
 
+    // default ENV is UAT
+    process.env['ENV'] = 'uat';
+
     // env config
     const envConfig = {
-    path: `./utils/env/.env.uat`,
+    path: `./utils/env/.env.${process.env.ENV}`,
+    // path: `./utils/env/.env.uat`,
     override: true
     }
     
-    console.log(`${process.env.ENV}`);
+    // console.log(`${process.env.ENV}`);
     // setup env config
-    require('dotenv').config(envConfig);
+    // require('dotenv').config(envConfig);
 
     // decrypt en
-    // require('@tka85/dotenvenc').decrypt({ encryptedFile: `./utils/env/.${process.env.ENV}.enc`});
+    require('@tka85/dotenvenc').decrypt({ encryptedFile: `./utils/env/.${process.env.ENV}.enc`});
 
     // test get env var
     // console.log(process.env.BASE_URL);
