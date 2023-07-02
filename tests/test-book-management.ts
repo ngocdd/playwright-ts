ef6dd674dc209fb30f1419bbc1fb1fa772384312
ngocdd <ngocdd94@gmail.com>
2023-07-02 23:48:57 +0700

    format code

--- tests/test-book-management.ts
+++ tests/test-book-management.ts
@@ -10,97 +10,112 @@ let bookMngPage : BookManagementPage;
 test.describe('test Book Management',async () => {
 
   test.beforeEach(async ({page}) => {
-    // initial new pages
+    // INITIAL
     loginPage = new LoginPage(page);
     bookMngPage = new BookManagementPage(page);
+
+    // PRECONDITIONS
     await loginPage.login();
   });
   
   test('test create new book', async ({ page }) => {
+    // INITIAL
     let bookName = await generateUUID('Book');
 
-    // console.log(bookName);
+    // PRECONDITIONS
+    // STEPS
     await bookMngPage.gotoBookManagement();
     await bookMngPage.addNewBook(bookName);
+    
+    // ASSERTIONS
     await expect(bookMngPage.snbMessage).toHaveText('You have created a new book successfully');
     await expect(bookMngPage.mnuBookName(bookName)).toContainText(bookName);
   });
 
   test('test create new chapter', async ({ page }) => {
+     // INITIAL
     let bookName = await generateUUID('Book');
     let chapterName = await generateUUID('Chapter');
 
-    // console.log(bookName);
+    // PRECONDITIONS
     await bookMngPage.gotoBookManagement();
     await bookMngPage.addNewBook(bookName);
     await bookMngPage.gotoBookDetail(bookName);
+    
+    // STEPS
     await bookMngPage.addNewChapter(chapterName);
-
+    
+    // ASSERTIONS
     await expect(bookMngPage.snbMessage.last()).toHaveText('You have added chapter successfully');
     await expect(bookMngPage.mnuChapter(chapterName)).toHaveText(chapterName);
   }); 
 
   test('test create new topic', async ({ page }) => {
+    // INITIAL
     let bookName = await generateUUID('Book');
     let chapterName = await generateUUID('Chapter');
     let topicName = await generateUUID('Topic');
-
-    // console.log(bookName);
+    
+    // PRECONDITIONS
     await bookMngPage.gotoBookManagement();
     await bookMngPage.addNewBook(bookName);
     await bookMngPage.gotoBookDetail(bookName);
     await bookMngPage.addNewChapter(chapterName);
-    await bookMngPage.addNewTopic(chapterName, topicName);
 
+    // STEPS
+    await bookMngPage.addNewTopic(chapterName, topicName);
+    
+    // ASSERTIONS
     await expect(bookMngPage.snbMessage.last()).toHaveText('You have added topic successfully');
     await expect(bookMngPage.mnuTopic(topicName)).toHaveText(topicName);
   }); 
 
   test('test create new LO', async ({ page }) => {
+    // INITIAL
     let bookName = await generateUUID('Book');
     let chapterName = await generateUUID('Chapter');
     let topicName = await generateUUID('Topic');
     let loName = await generateUUID('LO');
-
-    // console.log(bookName);
+    
+    // PRECONDITIONS
     await bookMngPage.gotoBookManagement();
     await bookMngPage.addNewBook(bookName);
     await bookMngPage.gotoBookDetail(bookName);
     await bookMngPage.addNewChapter(chapterName);
     await bookMngPage.addNewTopic(chapterName, topicName);
+
+    // STEPS
     await bookMngPage.addNewLO(topicName, LOType.LO, loName);
     await bookMngPage.backtoTopicDetail();
-
+    
+    // ASSERTIONS
     await expect(bookMngPage.snbMessage.last()).toHaveText('You have created a new LO successfully');
     await expect(bookMngPage.mnuLO(loName)).toHaveText(loName);
-    // await page.pause();
   }); 
 
 
   test('test move chapter', async ({ page }) => {
-    // initial
+    // INITIAL
     let bookName = await generateUUID('Book');
     let chapterName1 = await generateUUID('Chapter1');
     let chapterName2 = await generateUUID('Chapter2');
     let chapterName3 = await generateUUID('Chapter3');
-
-    // precondition
+    
+    // PRECONDITIONS
     await bookMngPage.gotoBookManagement();
     await bookMngPage.addNewBook(bookName);
     await bookMngPage.gotoBookDetail(bookName);
     await bookMngPage.addNewChapter(chapterName1);
     await bookMngPage.addNewChapter(chapterName2);
     await bookMngPage.addNewChapter(chapterName3);
-
-    // steps
+    
+    // STEPS
     await bookMngPage.moveChapter(chapterName1, MoveDirection.Down);
     await bookMngPage.moveChapter(chapterName3, MoveDirection.Up);
     
-    // Assertion
+    // ASSERTIONS
     await expect(bookMngPage.btnMoveChapterDown(chapterName3)).toBeEnabled();
-    const listOriginalChapter = await bookMngPage.lstChapter.all();
-    // console.log(listOriginalChapter);
-    
+    const listOriginalChapter = await bookMngPage.lstChapter.all();    
     let afterMove = [];
     for(let i =0; i < listOriginalChapter.length; i++){
       const name = await listOriginalChapter[i].textContent();
@@ -109,11 +124,9 @@ test.describe('test Book Management',async () => {
     await expect(afterMove[0]).toEqual(chapterName2);
     await expect(afterMove[1]).toEqual(chapterName3);
     await expect(afterMove[2]).toEqual(chapterName1);
-    // console.log(afterMove);
-    
   }); 
 
-  test.only('test move topic', async ({ page }) => {
+  test('test move topic', async ({ page }) => {
     // INITIAL
     let bookName = await generateUUID('Book');
     let chapterName = await generateUUID('Chapter');
@@ -135,7 +148,7 @@ test.describe('test Book Management',async () => {
     await bookMngPage.moveTopic(topicName3, MoveDirection.Up);
 
 
-    // ASSERTION
+    // ASSERTIONS
     await expect(await bookMngPage.btnMoveTopicUp(topicName3)).toBeEnabled();
     const listOriginalChapter = await bookMngPage.lstTopic(chapterName).all();
     
@@ -147,7 +160,6 @@ test.describe('test Book Management',async () => {
     await expect(afterMove[0]).toEqual(topicName2);
     await expect(afterMove[1]).toEqual(topicName3);
     await expect(afterMove[2]).toEqual(topicName1);
-
   }); 
 
 
