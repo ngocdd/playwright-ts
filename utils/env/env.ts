cd8c9be318d01d67b1d908b32e7ce7011765b1b3
ngocdd <ngocdd94@gmail.com>
2023-07-03 10:11:36 +0700

    update env config

--- utils/env/env.ts
+++ utils/env/env.ts
@@ -2,13 +2,15 @@
  * @Author                : ngocdd<ngocdd94@gmail.com>                        *
  * @CreatedDate           : 2023-07-03 09:42:33                               *
  * @LastEditors           : ngocdd<ngocdd94@gmail.com>                        *
- * @LastEditDate          : 2023-07-03 09:42:42                               *
+ * @LastEditDate          : 2023-07-03 10:10:20                               *
  *****************************************************************************/
 
 export function setEnv (){
 
     // default ENV is UAT
-    process.env['ENV'] = 'stg';
+    if(!process.env.ENV){
+        process.env['ENV'] = 'stg';    
+    }
 
     // decrypt en
     require('@tka85/dotenvenc').decrypt({ encryptedFile: `./utils/env/.${process.env.ENV}.enc`});
