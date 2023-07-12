017e9497c0d5979185ea30410ff38dbc5ee786f6
ngocdd <ngocdd94@gmail.com>
2023-07-12 09:51:39 +0700

    * chore(actions.ts): update created date and last edit date in file header * feat(actions.ts): add new methods for uploading files

--- utils/actions/actions.ts
+++ utils/actions/actions.ts
@@ -1,8 +1,8 @@
 /**
 @Author                : ngocdd<ngocdd94@gmail.com>
-@CreatedDate           : 2023-07-10 21:58:57
+@CreatedDate           : 2023-07-10 21:58:00
 @LastEditors           : ngocdd<ngocdd94@gmail.com>
-@LastEditDate          : 2023-07-10 21:58:57
+@LastEditDate          : 2023-07-12 09:51:23
 */
 
 import { test, Page, Locator } from '@playwright/test'
@@ -29,6 +29,20 @@ export default class Actions {
     await test.step(description, async () => {
       Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
       await locator.click()
+    })
+  }
+
+  public async uploadFile(locator: any, filePath: string, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await locator.setInputFiles(filePath)
+    })
+  }
+
+  public async uploadFiles(locator: any, listFilePath: [], description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await locator.setInputFiles(listFilePath)
     })
   }
 }
