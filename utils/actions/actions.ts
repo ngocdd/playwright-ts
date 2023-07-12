8ecfdfb1e7d4bf5d2efc95f9d66aecde2723d228
ngocdd <ngocdd94@gmail.com>
2023-07-12 10:03:00 +0700

    ADDED: Updated actions.ts file Added semicolons to import statements - Added Logger import statement - Added public methods for goto, input, upload, and uploadFiles - Added logging for each method  Updated method implementations to include logging and await statements

--- utils/actions/actions.ts
+++ utils/actions/actions.ts
@@ -5,44 +5,44 @@
 @LastEditDate          : 2023-07-12 09:51:23
 */
 
-import { test, Page, Locator } from '@playwright/test'
-import Logger from '../logger/logger'
+import { test, Page, Locator } from '@playwright/test';
+import Logger from '../logger/logger';
 
 export default class Actions {
   constructor(protected page: Page) {}
 
   public async goto(URL: string, description: string) {
     await test.step(description, async () => {
-      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
-      await this.page.goto(URL)
-    })
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description);
+      await this.page.goto(URL);
+    });
   }
 
   public async input(locator: Locator, text: string, description: string) {
     await test.step(description, async () => {
-      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
-      await locator.fill(text)
-    })
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description);
+      await locator.fill(text);
+    });
   }
 
   public async click(locator: any, description: string) {
     await test.step(description, async () => {
-      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
-      await locator.click()
-    })
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description);
+      await locator.click();
+    });
   }
 
   public async uploadFile(locator: any, filePath: string, description: string) {
     await test.step(description, async () => {
-      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
-      await locator.setInputFiles(filePath)
-    })
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description);
+      await locator.setInputFiles(filePath);
+    });
   }
 
   public async uploadFiles(locator: any, listFilePath: [], description: string) {
     await test.step(description, async () => {
-      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
-      await locator.setInputFiles(listFilePath)
-    })
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description);
+      await locator.setInputFiles(listFilePath);
+    });
   }
 }
