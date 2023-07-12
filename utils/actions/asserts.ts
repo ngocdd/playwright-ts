f460900e754912037cb0997e760cdb4d3cf7da47
ngocdd <ngocdd94@gmail.com>
2023-07-12 09:36:41 +0700

    * chore(asserts.ts): update created and last edit dates in file header * feat(asserts.ts): add new assertion methods for various element states

--- utils/actions/asserts.ts
+++ utils/actions/asserts.ts
@@ -1,8 +1,8 @@
 /**
 @Author                : ngocdd<ngocdd94@gmail.com>
-@CreatedDate           : 2023-07-10 21:59:02
+@CreatedDate           : 2023-07-10 21:59:00
 @LastEditors           : ngocdd<ngocdd94@gmail.com>
-@LastEditDate          : 2023-07-10 21:59:02
+@LastEditDate          : 2023-07-12 09:36:29
 */
 
 import { Page, test, expect } from '@playwright/test'
@@ -18,10 +18,80 @@ export default class Asserts {
     })
   }
 
-  public async toEnable(locator: any, description: string) {
+  public async toContainText(locator: any, expectedText: string, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toContainText(expectedText)
+    })
+  }
+
+  public async toBeEnable(locator: any, description: string) {
     await test.step(description, async () => {
       Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
       await expect(locator).toBeEnabled()
+    })
+  }
+
+  public async toBeDisabled(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeDisabled()
+    })
+  }
+
+  public async toBeAttached(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeAttached()
+    })
+  }
+
+  public async toBeChecked(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeChecked()
+    })
+  }
+
+  public async toBeEditable(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeEditable()
+    })
+  }
+
+  public async toBeEmpty(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeEmpty()
+    })
+  }
+
+  public async toBeFocused(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeFocused()
+    })
+  }
+
+  public async toBeHidden(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeHidden()
+    })
+  }
+
+  public async toBeInViewport(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeInViewport()
+    })
+  }
+
+  public async toBeVisible(locator: any, description: string) {
+    await test.step(description, async () => {
+      Logger.info(`worker ${process.env.TEST_WORKER_INDEX}: ` + description)
+      await expect(locator).toBeVisible()
     })
   }
 }
