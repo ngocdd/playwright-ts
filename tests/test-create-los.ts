3e3911362fbec56f4eef732e48b571424b054519
ngocdd <ngocdd94@gmail.com>
2023-07-12 10:47:52 +0700

    UPDATED: Added for creating a new LO and test case for questions for LO.
    - Created a new test case to test the creation of a new LO.
    - Added preconditions to create a book, chapter, and topic.
    - Added steps to add a new LO to the topic.
    - Asserted that the LO was created successfully and the LO name is displayed correctly.
    
    - Created a new test case to test the questions for the LO.
    - Added preconditions to create a book, chapter, and topic.
    - Added steps to add a new LO to the topic.
    - Asserted that the LO was created successfully and the LO name is displayed correctly.

---
+++ tests/test-create-los.ts
@@ -0,0 +1,87 @@
+/**
+@Author                : ngocdd<ngocdd94@gmail.com>
+@CreatedDate           : 2023-07-10 21:58:00
+@LastEditors           : ngocdd<ngocdd94@gmail.com>
+@LastEditDate          : 2023-07-12 10:47:38
+*/
+
+import { test } from '@playwright/test';
+import BookManagementPage from '../page-objects/book-management-page';
+import { generateUUID } from '../utils/data-provider/data-provider';
+import { LOType, MoveDirection } from '../utils/enumeration/enumeration';
+import LoginPage from '../page-objects/login-page';
+
+let loginPage: LoginPage;
+let bookMngPage: BookManagementPage;
+
+test.describe('test Book Management', async () => {
+  test.beforeEach(async ({ page }) => {
+    // INITIAL
+    loginPage = new LoginPage(page);
+    bookMngPage = new BookManagementPage(page);
+
+    // PRECONDITIONS
+    await loginPage.login();
+  });
+
+
+
+  test('test create new LO', async ({ page }) => {
+    // INITIAL
+    let bookName = await generateUUID('Book');
+    let chapterName = await generateUUID('Chapter');
+    let topicName = await generateUUID('Topic');
+    let loName = await generateUUID('LO');
+
+    // PRECONDITIONS
+    await bookMngPage.gotoBookManagement();
+    await bookMngPage.addNewBook(bookName);
+    await bookMngPage.gotoBookDetail(bookName);
+    await bookMngPage.addNewChapter(chapterName);
+    await bookMngPage.addNewTopic(chapterName, topicName);
+
+    // STEPS
+    await bookMngPage.addNewLO(topicName, LOType.LO, loName);
+    await bookMngPage.backToTopicDetail();
+
+    // ASSERTIONS
+    await bookMngPage.asserts.toHaveText(
+      bookMngPage.snbMessage.last(),
+      'You have created a new LO successfully',
+      `check notification create LO successfully`
+    );
+    await bookMngPage.asserts.toHaveText(bookMngPage.mnuLO(loName), loName, `check lo name is ${loName} created`);
+  });
+
+   test('test questions for LO', async ({ page }) => {
+    // INITIAL
+    let bookName = await generateUUID('Book');
+    let chapterName = await generateUUID('Chapter');
+    let topicName = await generateUUID('Topic');
+    let loName = await generateUUID('LO');
+
+    // PRECONDITIONS
+    await bookMngPage.gotoBookManagement();
+    await bookMngPage.addNewBook(bookName);
+    await bookMngPage.gotoBookDetail(bookName);
+    await bookMngPage.addNewChapter(chapterName);
+    await bookMngPage.addNewTopic(chapterName, topicName);
+
+    // STEPS
+    await bookMngPage.addNewLO(topicName, LOType.LO, loName);
+    await bookMngPage.backToTopicDetail();
+
+    // ASSERTIONS
+    await bookMngPage.asserts.toHaveText(
+      bookMngPage.snbMessage.last(),
+      'You have created a new LO successfully',
+      `check notification create LO successfully`
+    );
+    await bookMngPage.asserts.toHaveText(bookMngPage.mnuLO(loName), loName, `check lo name is ${loName} created`);
+  });
+
+  
+
+
+ 
+});
