bdf222e1b527d19b8e93d82a7a7a249fbc45f3cc
ngocdd <ngocdd94@gmail.com>
2023-07-12 09:54:31 +0700

    * chore(auth.setup.ts): comment out unused imports and code * refactor(auth.setup.ts): comment out unused variables and functions

--- utils/auth/auth.setup.ts
+++ utils/auth/auth.setup.ts
@@ -1,28 +1,28 @@
 /**
 @Author                : ngocdd<ngocdd94@gmail.com>
-@CreatedDate           : 2023-07-10 21:59:08
+@CreatedDate           : 2023-07-10 21:59:00
 @LastEditors           : ngocdd<ngocdd94@gmail.com>
-@LastEditDate          : 2023-07-10 21:59:08
+@LastEditDate          : 2023-07-12 09:54:23
 */
 
-import { Browser, Page, chromium } from '@playwright/test'
-import LoginPage from '../../page-objects/login-page'
+// import { Browser, Page, chromium } from '@playwright/test'
+// import LoginPage from '../../page-objects/login-page'
 
-let loginPage: LoginPage
+// let loginPage: LoginPage
 
-async function authenticate() {
-  const browser: Browser = await chromium.launch({
-    headless: false,
-  })
-  const context = await browser.newContext()
-  const page: Page = await context.newPage()
-  loginPage = new LoginPage(page)
+// async function authenticate() {
+//   const browser: Browser = await chromium.launch({
+//     headless: true,
+//   })
+//   const context = await browser.newContext()
+//   const page: Page = await context.newPage()
+//   loginPage = new LoginPage(page)
 
-  await loginPage.login()
+//   await loginPage.login()
 
-  await page.context().storageState({ path: './utils/auth/admin.json' })
-  // Dont forget your clean up :)
-  await browser.close()
-}
+//   await page.context().storageState({ path: './utils/auth/admin.json' })
+//   // Dont forget your clean up :)
+//   await browser.close()
+// }
 
-export default authenticate
+// export default authenticate
